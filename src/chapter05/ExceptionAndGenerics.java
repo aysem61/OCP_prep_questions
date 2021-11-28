5c67aa6a217a7a86a7003aaf1d905d7cf5f09fcc
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-11-28 23:35:34 +0100

    Update ExceptionAndGenerics.java

--- src/chapter05/ExceptionAndGenerics.java
+++ src/chapter05/ExceptionAndGenerics.java
@@ -1,6 +1,5 @@
 package chapter05;
 
-
 // PAGE 166 Question 24
 
 import java.util.Collection;
@@ -17,5 +16,4 @@ public class ExceptionAndGenerics {
 	public static void main(String[] args) {
 		throwOne(List.of(new ArithmeticException()));
 	}
-
 }
