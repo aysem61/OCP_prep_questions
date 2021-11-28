ef2d32775bf78eb313335e495cc74cf2f3c5d163
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-11-28 23:36:31 +0100

    Update Newsstand.java

--- src/chapter05/Newsstand.java
+++ src/chapter05/Newsstand.java
@@ -5,7 +5,9 @@ import java.util.TreeSet;
 // PAGE 164 Question 17
 
 class Magazine{
+	
 	private String name;
+	
 	public Magazine(String name) {
 		this.name = name;
 	}
@@ -13,9 +15,11 @@ class Magazine{
 	public int compareTo(Magazine m) {
 		return name.compareTo(m.name);
 	}
+	
 	public String toString() {
 		return name;
 	}
+	
 //	@Override
 //	public int compareTo(Object o) {
 //		try {
@@ -25,14 +29,17 @@ class Magazine{
 //		}
 //		return name.compareTo(((Magazine)o).name);
 //	}
+	
 	/*
 	  Magazine does not implement "Comparable"
 	  Since TreeSet does not look to see if the object can be compared until runtime,
 	  it throws "ClassCastException" when add() method is called    
 	 */
+	
 }
 
 public class Newsstand {
+	
 	public static void main(String[] args) {
 		var set = new TreeSet<Magazine>();
 		set.add(new Magazine("highlights"));
