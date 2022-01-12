a1741df496c87999b7967bb15816ba22e8db9af7
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-01-12 22:14:28 +0100

    Update Question_82_ch06.java

--- src/chapter06/Question_82_ch06.java
+++ src/chapter06/Question_82_ch06.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Question_82_ch06 {
 	
 	public static void main(String[] args) {
+		
 		var p = List.of(new StringBuilder("hello"), new StringBuilder("goodbye"));
 		var q = p.parallelStream().reduce(0, 
 				(w,x) -> w + x.length(),
@@ -12,6 +13,7 @@ public class Question_82_ch06 {
 //				(w,x) -> w.length() + x.length(),
 //				(y,z) -> y.length() + z.length());
 		System.out.print(q);
+		
 	}
 
 }
