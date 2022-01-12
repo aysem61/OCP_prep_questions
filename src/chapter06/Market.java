acf5c7dd6c7a7bfd76f94ff92b7b08e4a716d786
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-01-12 22:12:56 +0100

    Update Market.java

--- src/chapter06/Market.java
+++ src/chapter06/Market.java
@@ -9,10 +9,12 @@ import java.util.function.DoubleConsumer;
 public class Market {
 	
 	private static void checkPrices(List<Double> prices, Consumer scanner) {
+		
 		prices.forEach(scanner);
 	}
 
 	public static void main(String[] args) {
+		
 		List<Double> prices = List.of(1.2, 6.5, 3.0);
 		checkPrices(prices, p -> {
 //			String result = p < 5 ? "Correct" : "Too high";   // ===> gives error, because "<" operator is not defined in
@@ -20,5 +22,6 @@ public class Market {
 			String result = p.equals(null) ? "Correct" : "Too high";
 			System.out.println(result);
 		});
+		
 	}
 }
