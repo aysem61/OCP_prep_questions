f69355d21ef7ba052fd678dec778b91aec6b8277
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-01-19 22:10:49 +0100

    Update Concat.java

--- src/chapter06/Concat.java
+++ src/chapter06/Concat.java
@@ -7,13 +7,17 @@ import java.util.List;
 public class Concat {
 	
 	public String concat1(List<String> values) {
+		
 		return values.parallelStream().reduce("a", (x,y)-> x+y, String::concat);  // reduce with 3 parameters returns Generic
 	}
+	
 	public String concat2(List<String> values) {
 		return values.parallelStream().reduce((w, z) -> z+w).get();				  // reduce with 3 parameters returns Optional
+		
 	}
 
 	public static void main(String... questions) {
+		
 		Concat c = new Concat();
 		var list = List.of("Cat", "Hat");
 		String x = c.concat1(list);
