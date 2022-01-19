79e0ed2dc66c326ec19e0d8e8aae2b1fb029dbbc
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-01-19 22:11:56 +0100

    Update Question_48_ch06.java

--- src/chapter06/Question_48_ch06.java
+++ src/chapter06/Question_48_ch06.java
@@ -7,13 +7,16 @@ import java.util.stream.Stream;
 
 public class Question_48_ch06 {
 	
+	
 	public static void main(String[] args) {
+		
 		Stream.iterate(1, x -> x + 1)
 		.limit(5)
 		.skip(2)
 		.peek(System.out::print)
 		.collect(Collectors.toList())		// Here with collect(), we create another list
 		.forEach(System.out::print);		// Here we use forEach on that list
+		
 	}
 
 }
